751b061d912d13253ee9147ce486635a70bed4bd
Tiago19480521 <146497077+Tiago19480521@users.noreply.github.com>
2026-01-03 15:03:48 -0300

    Primeiro registro

---
+++ Numeros-primos-01.js
@@ -0,0 +1,11 @@
+const avalPrimo = () => {
+
+}
+
+const limparCampos = () => {
+    document.getElementById('inp_numero').value = ""
+    document.getElementById('inp_resultado').value = ""
+
+    document.getElementById('inp_numero').focus()
+
+}
